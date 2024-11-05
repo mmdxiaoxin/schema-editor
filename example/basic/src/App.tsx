673e278658f9d306433bcd56f2669ce0178541b8
小新 <782446723@qq.com>
2024-11-05 19:08:52 +0800

    test: 插件导入测试

---
+++ example/basic/src/App.tsx
@@ -0,0 +1,13 @@
+import SchemaEditor from 'schema-editor';
+
+function App() {
+  return (
+    <>
+      <div>
+        <SchemaEditor message="测试" />
+      </div>
+    </>
+  );
+}
+
+export default App;
